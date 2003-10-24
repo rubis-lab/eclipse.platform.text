6edfb4840235d31b50429c6d1f61e4ef13a6913e
Dani Megert <dmegert>
2003-10-24 16:15:17 +0000

    - Background highlighting

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/AbstractTextEditor.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/AbstractTextEditor.java
@@ -4372,6 +4372,9 @@ public abstract class AbstractTextEditor extends EditorPart implements ITextEdit
 			
 		} else if (fStatusFields != null)
 			fStatusFields.remove(category);
+		
+		if (fIncrementalFindTarget != null && ITextEditorActionConstants.STATUS_CATEGORY_FIND_FIELD.equals(category))
+			fIncrementalFindTarget.setStatusField(field);
 	}
 	
 	/**
